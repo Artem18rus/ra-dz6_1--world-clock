559775add35a881d45736197b3508c5e677c5b55
Artem18rus <urart88@mail.ru>
2023-02-26 18:08:08 +0400

    2

--- src/components/FieldList/FieldList.js
+++ src/components/FieldList/FieldList.js
@@ -1,15 +1,17 @@
+import React from "react";
 import FieldItem from "../FieldItem/FieldItem";
 
-function FieldList({ fields }) {
-  if (fields.length === 0) return;
-  // console.log(fields);
-  return (
-    <ol className="result">
-      {fields.map((field) => (
-        <FieldItem key={field.id} field={field} />
-      ))}
-    </ol>
-  );
+class FieldList extends React.Component {
+  render() {
+    if (this.props.fields.length === 0) return;
+    // console.log(fields);
+    return (
+      <ol className="result">
+        {this.props.fields.map((field) => (
+          <FieldItem key={field.id} field={field} />
+        ))}
+      </ol>
+    );
+  }
 }
-
 export default FieldList;
